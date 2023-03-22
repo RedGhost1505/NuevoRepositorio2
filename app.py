1635e1e622f302f03b892b481f707739aa51d605
RedGhost1505 <joshalejandro117@gmail.com>
2023-03-22 14:06:20 -0600

    Cambios en la app

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import Flask, render_template, request, redirect
-import requests
+# ------------------------------------------------------------
+# Josua Alejandro Zamarrón Ramírez 
+# Marzo/22/2023
+# ------------------------------------------------------------
+
+from flask import Flask, render_template, request, redirect, abort
+from requests import get, post
+
+
 app= Flask(__name__)
 
 url = 'https://api-todo-list3.onrender.com/api/tasks'
@@ -7,27 +14,39 @@ url = 'https://api-todo-list3.onrender.com/api/tasks'
 @app.route('/', methods=['GET','POST'])
 
 def home():
+
+    complete_tasks= {}
+    incomplete_tasks={}
+
     if request.method== 'GET':
-        tasks = requests.get(url).json()['tasks']
-        completed=[]
-        incompleted=[]
-        for task in tasks:
-            if task['status']== True:
-                completed.append(task)
-            else:
-                incompleted.append(task)
+        try:
+            tasks = get(url+'tasks').json()
+        except:
+            abort(500)
         
-        print(f'COMPLETADAS: {completed}')
-        print(f'INCOMPLETED: {incompleted}')
+        for task in tasks['tasks']:
+            if task['estado']:
+                complete_tasks[task['id']] = task
+                complete_tasks[task['id']].pop('id')
 
-        response ={'completed': completed,
-                   'incompleted':incompleted}
-        return render_template('index.html', response=response)
-
-
-    else:  #Post
-        pass
-
+        return render_template('index.html', content={'complete_tasks':complete_tasks,'incomplete_tasks':incomplete_tasks},uncompleted_len=len(incomplete_tasks),completed_len=len(complete_tasks))
+    
+    else: #Post
+        post_data = {'name':request.form.get('name')}
+        try:
+            task = post(url+'append',data=post_data).json()
+        except:
+            abort(500)
+        
+        for task in tasks['tasks']:
+            if task['estado']:
+                complete_tasks[task['id']] = task
+                complete_tasks[task['id']].pop('id')
+            elif not task['estado']:
+                incomplete_tasks[task['id']] = task
+                incomplete_tasks[task['id']].pop('id')
+     
+        return render_template('index.html', content={'complete_tasks':complete_tasks,'incomplete_tasks':incomplete_tasks},uncompleted_len=len(incomplete_tasks),completed_len=len(complete_tasks))
 
 
 if __name__ == '__main__':
