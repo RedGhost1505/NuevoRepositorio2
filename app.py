4354f9c3b75e9bc6e033bd75e7147094e3bb4101
RedGhost1505 <joshalejandro117@gmail.com>
2023-03-22 12:54:13 -0600

    Nueva url para render

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template, request, redirect
-
+import requests
 app= Flask(__name__)
 
-url = 'https://api-todo-list-85ve.onrender.com/api/tasks'
+url = 'https://api-todo-list3.onrender.com/api/tasks'
 
 @app.route('/', methods=['GET','POST'])
+
 def home():
     if request.method== 'GET':
-        return render_template('index.html')
+        tasks = requests.get(url).json()['tasks']
+        completed=[]
+        incompleted=[]
+        for task in tasks:
+            if task['status']== True:
+                completed.append(task)
+            else:
+                incompleted.append(task)
+        
+        print(f'COMPLETADAS: {completed}')
+        print(f'INCOMPLETED: {incompleted}')
+
+        response ={'completed': completed,
+                   'incompleted':incompleted}
+        return render_template('index.html', response=response)
 
 
     else:  #Post
